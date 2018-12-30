ae0bcfb13ae824be2642ec5bf7e6de55883eede8
Mathieu <mathieux51@gmail.com>
2018-12-30 16:16:15 +0100

    Update sw

--- src/sw.js
+++ src/sw.js
@@ -2,15 +2,15 @@
 
 console.log("Hi sw.js")
 
-workbox.skipWaiting()
-workbox.clientsClaim()
+// workbox.skipWaiting()
+// workbox.clientsClaim()
 
-workbox.routing.registerRoute(
-  new RegExp("https:.*min.(css|js)"),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "cdn-cache"
-  })
-)
+// workbox.routing.registerRoute(
+//   new RegExp("https:.*min.(css|js)"),
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: "cdn-cache"
+//   })
+// )
 
 // workbox.routing.registerRoute(
 //   new RegExp("http://.*:4567.*.json"),
