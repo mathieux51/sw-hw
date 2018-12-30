6981167768b37ce05cc93a223566fb4ba71fa100
Mathieu <mathieux51@gmail.com>
2018-12-30 18:41:54 +0100

    Updates

---
+++ precache-manifest.fa431785f701fa2aaea5cb6e784eb91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0ef65e542887e2c5686",
+    "url": "/static/js/main.f0ef65e5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "f0ef65e542887e2c5686",
+    "url": "/static/css/main.ca292199.chunk.css"
+  },
+  {
+    "revision": "81ec22a400b7240bb8e1f80b884ccd0b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
-importScripts("/sw-hw/precache-manifest.3f1fc7be7c76cea06d15271790eff0f7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.fa431785f701fa2aaea5cb6e784eb91f.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable no-restricted-globals */
 
 console.log("Hi sw.js")
 
-workbox.skipWaiting()
-workbox.clientsClaim()
+// workbox.skipWaiting()
+// workbox.clientsClaim()
 
-workbox.routing.registerRoute(
-  new RegExp("https:.*min.(css|js)"),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: "cdn-cache"
-  })
-)
+// workbox.routing.registerRoute(
+//   new RegExp("https:.*min.(css|js)"),
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: "cdn-cache"
+//   })
+// )
 
 // workbox.routing.registerRoute(
 //   new RegExp("http://.*:4567.*.json"),
