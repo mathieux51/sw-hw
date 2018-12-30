aa10a3eeb6872ddb5ae4770f2185310549fcfcb4
Mathieu <mathieux51@gmail.com>
2018-12-30 15:57:30 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,9 @@
 importScripts("/sw-hw/precache-manifest.3f1fc7be7c76cea06d15271790eff0f7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable no-restricted-globals */
+
+console.log("Hi sw.js")
+
 workbox.skipWaiting()
 workbox.clientsClaim()
 
@@ -11,10 +14,10 @@ workbox.routing.registerRoute(
   })
 )
 
-workbox.routing.registerRoute(
-  new RegExp("http://.*:4567.*.json"),
-  workbox.strategies.networkFirst()
-)
+// workbox.routing.registerRoute(
+//   new RegExp("http://.*:4567.*.json"),
+//   workbox.strategies.networkFirst()
+// )
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
 
